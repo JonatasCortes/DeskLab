ee002ab8d8af2abe63f0269734f04c6dc1fa8c0e
Jonatas <joncortes.on@gmail.com>
2026-05-04 23:51:59 -0300

    FEAT: Allow methods passed to EventListener to access args and kwargs

--- src/labweb/event_listener.py
+++ src/labweb/event_listener.py
@@ -10,8 +10,8 @@ class EventListener(EventSensitiveEntity):
         self.set_condition(condition)
 
     def handle_event(self, event: Event, *args: Any, **kwargs: Any) -> None:
-        if self._trigger_condition():
-            self._trigger_actions()
+        if self._trigger_condition(*args, **kwargs, event=event):
+            self._trigger_actions(*args, **kwargs, event=event)
 
     def get_condition(self) -> Callable[..., bool]:
         return self.__condition
@@ -28,12 +28,12 @@ class EventListener(EventSensitiveEntity):
             return
         self.__action: list[Callable[..., Any]] = [action]
 
-    def _trigger_condition(self) -> bool:
-        return self.get_condition()()
+    def _trigger_condition(self, *args: Any, **kwargs: Any) -> bool:
+        return self.get_condition()(*args, **kwargs)
 
-    def _trigger_actions(self) -> None:
+    def _trigger_actions(self, *args: Any, **kwargs: Any) -> None:
         for action in self.get_actions():
-            action()
+            action(*args, **kwargs)
 
 
 class FirstTimeEventListener(EventListener):
