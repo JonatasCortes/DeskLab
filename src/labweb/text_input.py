26f5f8857705d5d33f6db5a8f69000e6ebcd8dfd
Jonatas <joncortes.on@gmail.com>
2026-05-13 11:21:42 -0300

    REFACTOR: not gonna lie, this is all AI shit, but it works and implements the class TextInput that i was struggling with

--- src/labweb/text_input.py
+++ src/labweb/text_input.py
@@ -1,151 +1,72 @@
-from typing import Any, Optional
-
 import pygame
-from pygame import Surface
-from src.labweb.entities import Entity, DimensionableEntity
-from src.labweb.text import Text
+import time
+import re
+from typing import Any, Optional, Union, Tuple
+from pygame import Surface, Rect
 from src.labweb.color import Color
-from src.labweb.containers.clickable_flexbox import ClickableFlexBox
-from src.labweb.containers.flexbox import FlexBox
 from src.labweb.system.mouse import Mouse
 from src.labweb.system.keyboard import KeyBoard
 from src.labweb.system.clipboard import ClipBoard
-import time
-
-
-class _TextInputCell(FlexBox):
-
-    def __init__(self, value: str | Text, max_width: int, max_height: int, background_color: Color | tuple[int, int, int] | str, text_color: Color | tuple[int, int, int] | str) -> None:
-        text = value if isinstance(value, Text) else Text(value,
-                                                          color=text_color)
-        text = text.maximize(max_width, max_height)
-        super().__init__(text.get_width(), text.get_height(), 0, 0, "ROW",
-                         "CENTER", "CENTER", 0, background_color, True)
-        self.__text = text
-        self.add(text)
-        self.__is_selected = False
-        self.__defauld_background_color = background_color
-        self.__default_text_color = text_color
-
-    def is_selected(self) -> bool:
-        return self.__is_selected
-
-    def switch_selection_status(self) -> None:
-        self.__is_selected = not self.__is_selected
-
-    def handle_selection(self) -> None:
-        if self.is_selected():
-            self.set_color("BLUE")
-            self.__text.set_color("WHITE")
-            return
-        self.set_color(self.__defauld_background_color)
-        self.__text.set_color(self.__default_text_color)
-
-
-class _TextInputContainer(FlexBox):
-
-    def __init__(self,
-                 width: int,
-                 height: int,
-                 corners_radius: tuple[int, int, int, int] | int,
-                 color: Color | tuple[int, int, int] | str,
-                 text_color: Color | tuple[int, int, int] | str) -> None:
-
-        super().__init__(width, height, 0, 0, "ROW", "LEFT",
-                         "CENTER", corners_radius, color, True)
-        self.set_text_color(text_color)
-
-    def set_text_color(self, color: Color | tuple[int, int, int] | str):
-        self.__text_color = color if isinstance(color, Color) else Color(color)
-
-    def get_text_color(self) -> Color:
-        return self.__text_color
-
-    def add_cell(self, text: str) -> None:
-        cell = _TextInputCell(text, self.get_width(),
-                              self.get_height(),
-                              self.get_color(),
-                              self.get_text_color())
-        self.add(cell)
-
-    def force_cell_append(self, text: str):
-        while True:
-            try:
-                self.add_cell(text)
-                return
-            except ValueError:
-                new_cells_list = self.get_children()[1::]
-                self.set_children(new_cells_list)
-
-    def delete_last_word(self):
-        while len(self.get_children()) > 0:
-            removed_cell = self.pop()
-            char = self.__retrieve_character_from_cell(removed_cell)
-            if char == " ":
-                break
-
-    def __retrieve_character_from_cell(self, cell: Optional[Entity]) -> Optional[str]:
-        if not isinstance(cell, _TextInputCell):
-            return
-
-        removed_text_instance = cell.pop()
-        if not isinstance(removed_text_instance, Text):
-            return
-
-        return removed_text_instance.get_text()
+from src.labweb.containers.clickable_flexbox import ClickableFlexBox
+from src.labweb.text import Text
 
 
 class TextInput(ClickableFlexBox):
-
-    __HORIZONTAL_PADDING_PERCENTAGE = 5
-    __VERTICAL_PADDING_PERCENTAGE = 33
-    __CURSOR_HIDING_DELAY = 0.5
-
     def __init__(self,
                  width: int,
                  height: int,
-                 corners_radius: tuple[int, int, int, int] | int = 0,
-                 background_color: Color | tuple[int,
-                                                 int, int] | str = "WHITE",
-                 text_color: Color | tuple[int, int, int] | str = "BLACK") -> None:
+                 corners_radius: Union[Tuple[int, int, int, int], int] = 0,
+                 background_color: Union[Color,
+                                         Tuple[int, int, int], str] = "WHITE",
+                 text_color: Union[Color, Tuple[int, int, int], str] = "BLACK") -> None:
 
-        super().__init__(width, height, 0, 0, "ROW", "CENTER", "CENTER",
-                         corners_radius, background_color, True)
-        self.__set_text_container(text_color)
-        self.__is_focused = False
-        self.__text = ""
-        self.__cursor_index = 0
-        self.__display_cursor = False
-        self.__last_key_press_time = time.time()
+        super().__init__(width, height, 0, 0, "ROW", "LEFT",
+                         "CENTER", corners_radius, background_color, True)
 
-    def is_focused(self) -> bool:
-        return self.__is_focused
+        text = Text("", color=text_color, font="arial")
+        self.__text: Text = text.maximize(99999, int(height*0.5))
 
-    def __get_cursor_rect(self) -> tuple[int, ...]:
+        if isinstance(text_color, Color):
+            self.__text_color: Color = text_color
+        else:
+            self.__text_color: Color = Color(text_color)
 
-        cursor_y = self.__text_container.get_y()
-        cursor_x = self.__text_container.get_x()
-        for letter in self.__text_container.get_children()[:self.__cursor_index+1]:
-            if isinstance(letter, DimensionableEntity):
-                cursor_x += letter.get_width()
-        cursor_width = 2
-        cursor_height = self.__text_container.get_height()
+        # Índices e Seleção
+        self.__cursor_index: int = 0
+        self.__selection_anchor: Optional[int] = None
+        self.__is_focused: bool = False
 
-        return (cursor_x, cursor_y, cursor_width, cursor_height)
+        # Visual e Scroll
+        self.__scroll_offset: int = 0
+        self.__display_cursor: bool = True
+        self.__last_cursor_toggle: float = time.time()
+        self.__cursor_blink_speed: float = 0.5
 
-    def __set_text_container(self, text_color: Color | tuple[int, int, int] | str) -> None:
-        self.__text_container = _TextInputContainer(self.get_width() - self.__calcuate_horizontal_padding(),
-                                                    self.get_height() - self.__calculate_vertical_padding(),
-                                                    self.get_corners_radius(), self.get_color(), text_color)
-        self._add(self.__text_container)
+        # Timers para cliques múltiplos
+        self.__last_click_time: float = 0.0
+        self.__click_count: int = 0
 
-    def __calcuate_horizontal_padding(self) -> int:
-        return self.__HORIZONTAL_PADDING_PERCENTAGE*self.get_width()//100
+    def __get_x_at_index(self, index: int) -> int:
+        substring: str = self.__text.get_text()[:index]
+        return self.__text.get_font().size(substring)[0]
 
-    def __calculate_vertical_padding(self) -> int:
-        return self.__VERTICAL_PADDING_PERCENTAGE*self.get_height()//100
+    def __get_index_from_pos(self, mouse_x: int) -> int:
+        """Retorna o índice do caractere mais próximo da posição X do mouse."""
+        relative_x: int = mouse_x - self.get_x() + self.__scroll_offset
 
-    def handle_event(self, *args: Any, **kwargs: Any):
+        best_index: int = 0
+        min_diff: float = float('inf')
+
+        for i in range(len(self.__text.get_text()) + 1):
+            char_x: int = self.__get_x_at_index(i)
+            diff: float = abs(char_x - relative_x)
+            if diff < min_diff:
+                min_diff = diff
+                best_index = i
+        return best_index
+
+    def handle_event(self, *args: Any, **kwargs: Any) -> None:
+
         super().handle_event(*args, **kwargs)
 
         mouse = kwargs.get("mouse")
@@ -157,73 +78,202 @@ class TextInput(ClickableFlexBox):
                                f"Expected a {KeyBoard.__name__} instance in kwargs with key 'keyboard'",
                                f"Expected a {ClipBoard.__name__} instance in kwargs with key 'clipboard'")
 
-        self.__add_focus_listener(mouse)
-        self.__add_typing_listener(keyboard)
-        self.__add_delete_listener(keyboard)
-        self.__add_cursor_move_listener(keyboard)
-        self.__add_cursor_display_listener(keyboard)
+        if not all([mouse, keyboard, clipboard]):
+            return
 
-    def __add_focus_listener(self, mouse: Mouse):
+        # 1. Gerenciar Foco
         if self.is_clicked():
             self.__is_focused = True
-            return
+            self.__handle_click_logic(mouse)
         elif mouse.is_clicked():
             self.__is_focused = False
 
-    def __add_typing_listener(self, keyboard: KeyBoard):
-        if not self.is_focused():
-            return
-        if keyboard.any_text_input():
-            text = keyboard.last_input()
-            if not text:
-                return
-            self.__text_container.force_cell_append(text)
-            self.__text += text
-            self.__cursor_index += len(text)
-
-    def __add_delete_listener(self, keyboard: KeyBoard):
-
-        if not self.is_focused() or not keyboard.key_down("backspace"):
-            return
-        elif not keyboard.ctrl_active() and not keyboard.meta_active():
-            self.__text_container.pop()
-            self.__text = self.__text[:-1]
-            self.__cursor_index -= 1
-        else:
-            last_word = self.__text.split(" ")[-1]
-            self.__cursor_index -= len(last_word)
-            self.__text = " ".join(self.__text.split(" ")[:-1])
-            self.__text_container.delete_last_word()
-
-    def __add_cursor_move_listener(self, keyboard: KeyBoard):
-        if not self.is_focused():
-            return
-        if keyboard.key_down("left"):
-            self.__cursor_index -= 1
-        elif keyboard.key_down("right"):
-            self.__cursor_index += 1
-
-    def __add_cursor_display_listener(self, keyboard: KeyBoard):
-        current_time = time.time()
-
-        if not self.is_focused():
-            self.__display_cursor = False
+        if not self.__is_focused:
             return
 
+        # 2. Seleção via Mouse (Drag)
+        if mouse.is_held() and not self.is_clicked():
+            self.__cursor_index = self.__get_index_from_pos(
+                mouse.get_position()[0])
+            if self.__selection_anchor is None:
+                self.__selection_anchor = self.__cursor_index
+
+        # 3. Teclado
         if keyboard.any_key_pressed():
-            self.__last_key_press_time = current_time
-            self.__display_cursor = True
+            self.__handle_keyboard(keyboard, clipboard)
+            self.__reset_cursor_blink()
+
+        self.__update_scroll()
+
+    def __handle_click_logic(self, mouse: Mouse) -> None:
+        now: float = time.time()
+        idx: int = self.__get_index_from_pos(mouse.get_position()[0])
+
+        if now - self.__last_click_time < 0.4:
+            self.__click_count += 1
+        else:
+            self.__click_count = 1
+        self.__last_click_time = now
+
+        if self.__click_count == 1:
+            self.__cursor_index = idx
+            self.__selection_anchor = idx
+        elif self.__click_count == 2:
+            self.__select_word_at(idx)
+        elif self.__click_count >= 3:
+            self.__select_all()
+
+    def __handle_keyboard(self, kb: KeyBoard, cb: ClipBoard) -> None:
+        shift_active: bool = kb.shift_active()
+
+        if kb.ctrl_active() or kb.meta_active():
+            if kb.key_down("a"):
+                self.__select_all()
+            elif kb.key_down("c"):
+                self.__copy(cb)
+            elif kb.key_down("v"):
+                self.__paste(cb)
+            elif kb.key_down("x"):
+                self.__cut(cb)
+            elif kb.key_down("left"):
+                self.__move_cursor_word(-1, shift_active)
+            elif kb.key_down("right"):
+                self.__move_cursor_word(1, shift_active)
             return
 
-        time_since_last_press = current_time - self.__last_key_press_time
+        if kb.key_down("left"):
+            self.__move_cursor(-1, shift_active)
+        elif kb.key_down("right"):
+            self.__move_cursor(1, shift_active)
+        elif kb.key_down("backspace"):
+            self.__delete_text()
+        elif kb.any_text_input() and (text := kb.last_input()):
+            self.__insert_text(text)
 
-        if time_since_last_press <= self.__CURSOR_HIDING_DELAY:
-            self.__display_cursor = True
+    def __insert_text(self, string: str) -> None:
+        self.__remove_selected_region()
+        self.__text = self.__text.sub(end=self.__cursor_index) + \
+            string + self.__text.get_text()[self.__cursor_index:]
+        self.__cursor_index += len(string)
+        self.__selection_anchor = None
+
+    def __delete_text(self) -> None:
+        if self.__selection_anchor is not None and self.__selection_anchor != self.__cursor_index:
+            self.__remove_selected_region()
+        elif self.__cursor_index > 0:
+            self.__text = self.__text.sub(end=self.__cursor_index - 1) + \
+                self.__text.get_text()[self.__cursor_index:]
+            self.__cursor_index -= 1
+
+    def __remove_selected_region(self) -> None:
+        if self.__selection_anchor is None or self.__selection_anchor == self.__cursor_index:
             return
+        start, end = sorted((self.__selection_anchor, self.__cursor_index))
+        self.__text = self.__text.sub(end=start) + self.__text.get_text()[end:]
+        self.__cursor_index = start
+        self.__selection_anchor = None
 
-        self.__display_cursor = int(current_time * 1.5) % 2 == 0
+    def __move_cursor(self, delta: int, shift: bool) -> None:
+        if not shift:
+            self.__selection_anchor = None
+        elif self.__selection_anchor is None:
+            self.__selection_anchor = self.__cursor_index
+
+        self.__cursor_index = max(
+            0, min(len(self.__text), self.__cursor_index + delta))
+
+    def __move_cursor_word(self, direction: int, shift: bool) -> None:
+        if not shift:
+            self.__selection_anchor = None
+        elif self.__selection_anchor is None:
+            self.__selection_anchor = self.__cursor_index
+
+        if direction > 0:
+            match = re.search(r'\w\W', self.__text.get_text()
+                              [self.__cursor_index:])
+            self.__cursor_index += (match.end()
+                                    if match else len(self.__text) - self.__cursor_index)
+        else:
+            match = re.search(r'\w\W', self.__text.get_text()
+                              [:self.__cursor_index][::-1])
+            self.__cursor_index -= (match.end()
+                                    if match else self.__cursor_index)
+
+    def __select_word_at(self, index: int) -> None:
+        # Busca limites de espaços ou pontuação ao redor do índice
+        text_around: str = self.__text.get_text()
+        start: int = index
+        while start > 0 and text_around[start-1].isalnum():
+            start -= 1
+        end: int = index
+        while end < len(text_around) and text_around[end].isalnum():
+            end += 1
+        self.__selection_anchor = start
+        self.__cursor_index = end
+
+    def __select_all(self) -> None:
+        self.__selection_anchor = 0
+        self.__cursor_index = len(self.__text)
+
+    def __copy(self, cb: ClipBoard) -> None:
+        if self.__selection_anchor is not None:
+            start, end = sorted((self.__selection_anchor, self.__cursor_index))
+            cb.put_text(self.__text.sub(start, end))
+
+    def __paste(self, cb: ClipBoard) -> None:
+        self.__insert_text(cb.get_text())
+
+    def __cut(self, cb: ClipBoard) -> None:
+        self.__copy(cb)
+        self.__remove_selected_region()
+
+    def __update_scroll(self) -> None:
+        cursor_x: int = self.__get_x_at_index(self.__cursor_index)
+        padding: int = 20
+        if cursor_x - self.__scroll_offset > self.get_width() - padding:
+            self.__scroll_offset = cursor_x - self.get_width() + padding
+        elif cursor_x - self.__scroll_offset < padding:
+            self.__scroll_offset = max(0, cursor_x - padding)
 
     def display(self, screen: Surface) -> None:
         super().display(screen)
-        if self.__display_cursor:
-            pygame.draw.rect(screen, (0, 0, 0), self.__get_cursor_rect())
+
+        # Clip para evitar que o texto vaze o container
+        text_area: Rect = Rect(self.get_x(), self.get_y(),
+                               self.get_width(), self.get_height())
+        old_clip: Optional[Rect] = screen.get_clip()
+        screen.set_clip(text_area)
+
+        # Desenhar Seleção
+        if self.__selection_anchor is not None and self.__selection_anchor != self.__cursor_index:
+            start_x: int = self.__get_x_at_index(
+                min(self.__selection_anchor, self.__cursor_index))
+            end_x: int = self.__get_x_at_index(
+                max(self.__selection_anchor, self.__cursor_index))
+            sel_rect: Rect = Rect(self.get_x() + start_x - self.__scroll_offset,
+                                  self.get_y(), end_x - start_x, self.get_height())
+            pygame.draw.rect(screen, (173, 216, 230), sel_rect)
+
+        # Desenhar Texto Principal
+        text_surf: Surface = self.__text.get_font().render(
+            self.__text.get_text(), True, self.__text_color.get_tuple())
+        screen.blit(text_surf, (self.get_x() - self.__scroll_offset,
+                    self.get_y() + (self.get_height() - text_surf.get_height()) // 2))
+
+        # Desenhar Cursor
+        if self.__is_focused:
+            now: float = time.time()
+            if int((now - self.__last_cursor_toggle) / self.__cursor_blink_speed) % 2 == 0:
+                cx: int = self.get_x() + self.__get_x_at_index(self.__cursor_index) - \
+                    self.__scroll_offset
+                pygame.draw.line(screen, (0, 0, 0), (cx, self.get_y(
+                ) + 5), (cx, self.get_y() + self.get_height() - 5), 2)
+
+        screen.set_clip(old_clip)
+
+    def __reset_cursor_blink(self) -> None:
+        self.__last_cursor_toggle = time.time()
+
+    def get_text(self) -> str:
+        """Requisito da Issue: Permitir ao desenvolvedor recuperar o texto a qualquer momento."""
+        return self.__text.get_text()
